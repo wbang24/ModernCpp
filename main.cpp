c2fd16616df29ea525247362b117d5df9d9b2e35
Wayland Bang <wayland_bang@thecdm.ca>
2019-09-05 14:33:03 -0600

    empty project for practicing Scott Meyer's Modern C++ book

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ModernCpp
+//
+//  Created by Wayland Bang on 2019-09-05.
+//  Copyright © 2019 Wayland Bang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
